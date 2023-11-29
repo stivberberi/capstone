3234c11da20e23a97c708e10b5db7b5af943dd40
Stiv Berberi <stiv.berberi1@gmail.com>
2023-11-28 19:29:13 -0500

    Start adding buffer to store pressure data

--- main/capstone.c
+++ main/capstone.c
@@ -3,6 +3,7 @@
 #include "freertos/FreeRTOS.h"
 #include "portmacro.h"
 #include "pressure_sensor.h"
+#include "pump.h"
 #include <stdio.h>
 
 void app_main(void) {
@@ -11,20 +12,23 @@ void app_main(void) {
   TaskHandle_t read_ps_handle = NULL;
 
   // ADC and calibration handles for the pressure sensor:
-  adc_oneshot_unit_handle_t ps_adc_handle;
-  adc_cali_handle_t ps_cali_handle;
+  adc_oneshot_unit_handle_t ps_adc_handle = NULL;
+  adc_cali_handle_t ps_cali_handle = NULL;
   setup_ps_adc(&ps_adc_handle, &ps_cali_handle);
-  PsHandle ps_task_args = {
-      .ps_adc_handle = &ps_adc_handle,
-      .ps_cali_handle = &ps_cali_handle,
+
+  // Pressure sensor reading task
+  double ps_bufr[PS_BUFR_SIZE] = {0.0};
+  PsArgs ps_task_args = {
+      .ps_adc_handle = ps_adc_handle,
+      .ps_cali_handle = ps_cali_handle,
+      .pressure_buf = ps_bufr,
   };
   xTaskCreate(read_ps_adc, "Reading Pressure Sensor", 2048, &ps_task_args, 5,
               &read_ps_handle);
-  configASSERT(read_ps_handle);
 
   // test pump code
   setup_pump();
-  
+
   while (1) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
