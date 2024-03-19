97b9105b01d8c754c3e56544ad964e79dcf71d95
Stiv Berberi <stiv.berberi1@gmail.com>
2024-03-18 22:07:11 -0400

    Make pressure readings a debug log

--- components/pressure_sensor/pressure_sensor.c
+++ components/pressure_sensor/pressure_sensor.c
@@ -7,8 +7,8 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "hal/adc_types.h"
+#include "lcd_screen.h"
 #include "portmacro.h"
 
 const static char *TAG = "Pressure_Sensor"; // used as the tag for ESP_LOG's
@@ -70,11 +70,13 @@ void read_ps_adc(void *ps_args) {
       // https://docs.espressif.com/projects/esp-idf/en/v4.4/esp32/api-reference/peripherals/adc.html
       // Definitely might be wrong / needs updating / testing however.
       voltage = adc_raw_reading * 2450 / 4095;
-      ESP_LOGI(TAG, "Uncalibrated voltage: %d voltage", voltage);
+      ESP_LOGD(TAG, "Uncalibrated voltage: %d voltage", voltage);
     }
-    ESP_LOGI(TAG, "Converted pressure: %lf kPa\n",
-             convert_voltage_to_pressure(voltage));
-    vTaskDelay(10 / portTICK_PERIOD_MS);
+    double converted_voltage = convert_voltage_to_pressure(voltage);
+    ESP_LOGD(TAG, "Converted pressure: %lf kPa\n", converted_voltage);
+
+    // run once every 1000 ms.
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -92,9 +94,7 @@ void cleanup_ps_adc(PsHandle_Ptr ps_handles) {
   ESP_ERROR_CHECK(adc_oneshot_del_unit(*ps_handles->ps_adc_handle));
   if (ps_handles->ps_cali_handle != NULL) {
     ESP_LOGI(TAG, "Cleaning up ps calibration scheme...");
-  //   ESP_ERROR_CHECK(
-
-  //       adc_cali_delete_scheme_line_fitting(*ps_handles->ps_cali_handle));
-  // }
-}
+    ESP_ERROR_CHECK(
+        adc_cali_delete_scheme_line_fitting(*ps_handles->ps_cali_handle));
+  }
 }
